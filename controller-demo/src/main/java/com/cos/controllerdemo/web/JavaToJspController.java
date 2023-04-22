41724ae3af2f7421fdc1547038a043d72631869f
jissssoo1717 <jisoolee7101@gmail.com>
2023-04-22 13:15:49 +0900

    Jsp java code and redirection

--- controller-demo/src/main/java/com/cos/controllerdemo/web/JavaToJspController.java
+++ controller-demo/src/main/java/com/cos/controllerdemo/web/JavaToJspController.java
@@ -1,7 +1,11 @@
 package com.cos.controllerdemo.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.cos.controllerdemo.domain.User;
+
 
 @Controller
 public class JavaToJspController {
@@ -9,5 +13,19 @@ public class JavaToJspController {
 	@GetMapping("/jsp/java")
 	public String jspToJava() {
 		return "d";
+	}
+	
+	
+	@GetMapping("/jsp/java/model")
+	public String jspToJavaToModel(Model model) { 
+		// 함수의 파라미터에 Model을 선언
+		
+		User user = new User();
+		user.setUsername("ssar");
+		
+		// addAttribute 함수로 전달만 함.
+		model.addAttribute("username", user.getUsername());
+		
+		return "e";
 	}
 }
