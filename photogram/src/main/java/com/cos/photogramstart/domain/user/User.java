ef63458ab2cf69f42507a95874d092dd4d8a77cc
jissssoo1717 <jisoolee7101@gmail.com>
2023-04-23 16:03:40 +0900

    Set Password Hash

--- photogram/src/main/java/com/cos/photogramstart/domain/user/User.java
+++ photogram/src/main/java/com/cos/photogramstart/domain/user/User.java
@@ -2,6 +2,7 @@ package com.cos.photogramstart.domain.user;
 
 import java.time.LocalDateTime;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -9,12 +10,15 @@ import javax.persistence.Id;
 import javax.persistence.PrePersist;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 // JPA - Java Persistence API (자바로 데이터를 영구적으로 저장(DB)할 수 있는 API 제공)
 
 
+
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor // 빈 생성자
 @Data
@@ -25,6 +29,7 @@ public class User {
 	@GeneratedValue(strategy = GenerationType.IDENTITY) // 번호 증가 전략이 데이터베이스를 따라감
 	private int id;
 	
+	@Column(unique = true)
 	private String username;
 	private String password;
 	
--- photogram/src/main/java/com/cos/photogramstart/web/AuthController.java
+++ photogram/src/main/java/com/cos/photogramstart/web/AuthController.java
@@ -6,16 +6,28 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.cos.photogramstart.domain.user.User;
+import com.cos.photogramstart.service.AuthService;
 import com.cos.photogramstart.web.dto.auth.SignupDto;
 
+import lombok.RequiredArgsConstructor;
+
+
+@RequiredArgsConstructor // final 필드를 DI할 때 사용
 @Controller // 1.IoC 등록 2. File 리턴하는 컨트롤러
 public class AuthController {
 	
 	
 	private static final Logger log = LoggerFactory.getLogger(AuthController.class);
-
 	
+	private final AuthService authService;
 
+	/* 의존성 주입
+	public AuthController(AuthService authService) {
+		this.authService = authService;
+	}
+	 */
+	
 	@GetMapping("/auth/signin")
 	public String signinForm() {
 		
@@ -35,6 +47,13 @@ public class AuthController {
 	public String signup(SignupDto signupDto) { // key=value (x-www-form-urlencoded)
 		
 		log.info(signupDto.toString());
+		// User <- SignupDto에 있는 데이터 넣기
+		
+		User user = signupDto.toEntity();
+		log.info(user.toString());
+		
+		User userEntity = authService.회원가입(user);
+		System.out.println(userEntity);
 		
 		return "auth/signin";
 	}
